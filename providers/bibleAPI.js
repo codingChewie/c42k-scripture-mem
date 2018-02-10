513e84303c7b84905d826619ee81a9c281b1cbc0
Abby Fleming <abbyleighanne@gmail.com>
2018-02-10 11:04:19 -0600

    using labs API

--- providers/bibleAPI.js
+++ providers/bibleAPI.js
@@ -1,8 +1,8 @@
 const verseRequest = async (verseParams) => {
 
     console.log('verseParams', verseParams);
-    const response = await fetch(`https://bible-api.com/${verseParams}`);
-    //const response = await fetch('http://labs.bible.org/api/?passage=John%203:16');
+    //const response = await fetch(`https://bible-api.com/${verseParams}`);
+    const response = await fetch(`http://labs.bible.org/api/?passage=${verseParams}&type=json`);
     const json = await response.json();
     console.log(json);
 }

1c46c492f4e834280e4507fde8601ed7aa272215
webbdm <gwebb762@gmail.com>
2018-02-10 02:55:35 -0600

    Setup form

--- App.js
+++ App.js
@@ -1,13 +1,80 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Header, Input, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import mountain from './img/mountain.jpg';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      translation: '',
+      book: '',
+      chapter: '',
+      verse: '',
+      query: ''
+    };
+  }
+
+  searchForVerse = () => {
+    let search = `${this.state.book} ${this.state.chapter} : ${this.state.verse}, ${this.state.translation}`;
+    this.setState({ query: search });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1 }}>
+        <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'Scripturize', style: { color: '#fff', fontSize: 30 } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+          outerContainerStyles={{ width: '100%' }}
+        />
+        <View style={{ flex: 3, backgroundColor: 'skyblue' }}>
+          <TextInput
+            style={{ height: 40 }}
+            placeholder="Translation"
+            onChangeText={(text) => this.setState({ translation: text })}
+          />
+
+          <TextInput
+            style={{ height: 40 }}
+            placeholder="Book"
+            onChangeText={(text) => {
+              this.setState({ book: text });
+            }}
+          />
+
+          <TextInput
+            style={{ height: 40 }}
+            placeholder="Chapter"
+            onChangeText={(text) => this.setState({ chapter: text })}
+          />
+
+          <TextInput
+            style={{ height: 40 }}
+            placeholder="Verse"
+            onChangeText={(text) => this.setState({ verse: text })}
+          />
+
+          <Button
+            text='Search'
+            buttonStyle={{
+              backgroundColor: "rgba(92, 99,216, 1)",
+              width: 300,
+              height: 45,
+              borderColor: "transparent",
+              borderWidth: 0,
+              borderRadius: 5
+            }}
+            onPress={this.searchForVerse}
+          />
+
+          <Text style={{ padding: 10, fontSize: 42 }}>
+            {this.state.query}
+          </Text>
+        </View>
       </View>
     );
   }
@@ -16,8 +83,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });

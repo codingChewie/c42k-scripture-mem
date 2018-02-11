2b61e5243b85843996fdf8ef284bc15bd440a744
Alexander Clark <alexdclark93@gmail.com>
2018-02-11 12:23:11 -0600

    Modified request to not send footnotes
    Modifed Bible API to not send foot notes modified responses.

--- providers/bibleAPI.js
+++ providers/bibleAPI.js
@@ -1,13 +1,22 @@
 
 const verseRequest = async (verseParams) => {
 
-    const response = await fetch(`https://api.esv.org/v3/passage/text/?q=${verseParams}&include-passage-references=true&include-first-verse-numbers=true`, {
+    const response = await fetch(`https://api.esv.org/v3/passage/text/?q=${verseParams}&include-first-verse-numbers=true`, {
         method: 'GET',
         headers: {
             'Authorization': config.api_auth
         }
     })
     const json = await response.json();
+    var Canonical = json.canonical;
+
+    if (Canonical != verseParams){
+        console.log("Verse not found did you mean: " + Canonical +"?");
+    }
+    else{
+        console.log("Here is your verse:" + json.Passages[]);
+    }
+
     console.log(json);
 }
 
